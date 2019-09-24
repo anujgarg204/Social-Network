2fd819abcf8fd045623e4b01049032ade0e5debe
anujgarg204 <anujgarg204@gmail.com>
2019-09-24 17:20:37 +0530

    Validatiors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
-
 
 const app = express();
 
@@ -15,9 +15,11 @@ app.use(bodyParser.json())
 
 mongoose.connect('mongodb://localhost:27017/social', {useNewUrlParser: true}).then(() => console.log('mongo working')).catch(err => console.log(err));
 
-app.get('/',(req,res) => {
-    return res.send('Hello!')
-});
+//passport middleware
+app.use(passport.initialize())
+
+// passport config
+require('./config/passport.js')(passport)
 
 app.use('/api/users', users);
 app.use('/api/posts', posts);
